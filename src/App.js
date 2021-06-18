1f15d5f2da8adec6e5f360bdbaf44b7d095a41fe
Ghost Orchid <kelsie.cummings23@gmail.com>
2021-06-18 11:21:04 -0400

    added additional page routes to app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Create from "./pages/Create";
+import Edit from "./pages/edit";
+import ViewRecommendation from "./pages/ViewRecommenation";
+import ViewFavorite from "./pages/ViewFavorite";
 
 
 function App() {
@@ -14,6 +20,12 @@ function App() {
           <Switch>
             {/* <Route exact path="/" component={Home} /> */}
             <Route exact path="/login" component={Login} />
+            <Route exact path="/home" component={Home} />
+            <Route exact path="/profile" component={Profile} />
+            <Route path="/create" component={Create} />
+            <Route path="/edit" component={Edit} />
+            <Route path="/view-recommendation" component={ViewRecommendation} />
+            <Route path="/view-favorite" component={ViewFavorite} />
           </Switch>
         </div>
       </Router>
