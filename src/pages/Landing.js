bff03b7d810166f6ddc09fac702fb1925f47b0ad
Janeé Mays <janee.elise@gmail.com>
2021-06-18 20:36:28 -0400

    renamed file, added landing page skeleton

---
+++ src/pages/Landing.js
@@ -0,0 +1,18 @@
+import React from "react";
+import city from '../images/bw-city.jpg';
+
+
+const Landing = () => {
+    return(
+        <div>
+        {/* Image not rendering at the moment - needs fixed */}
+        {/* styles ={{ backgroundImage:`(${city})`}} */}
+        <header>
+        <h1>Show me the city!</h1>
+        </header>
+        <input type="text" placeholder="Search for a city.." name="search"></input>
+        </div>
+    )
+}
+
+export default Landing;
