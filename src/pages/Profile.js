25d48a0bebcc22e9770334fc40e2077602fb2baa
Jake <jakech617@gmail.com>
2021-06-18 20:11:59 -0400

    created starter profile page

---
+++ src/pages/Profile.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div>
+      <h1>My Profile</h1>
+      <p>Name: John Smith</p>
+      <p>Email: username@gmail.com</p>
+      <h1>My Recommendations</h1>
+      <h1>My Favorites</h1>
+    </div>
+  );
+};
+
+export default Profile;
