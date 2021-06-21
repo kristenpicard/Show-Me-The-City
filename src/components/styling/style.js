f73d57e761f06b581df948c1098b5525e386ee77
Kristen Picard <kristenpicard93@gmail.com>
2021-06-21 11:10:15 -0400

    Styling landing page with styled components

---
+++ src/components/styling/style.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+import img from "../../images/bw-city.jpg";
+
+export const Button = styled.button`
+  color: #76ead7;
+  background-color: #393e46;
+  width: 70%;
+`;
+
+export const H1 = styled.h1`
+  color: white;
+`;
+
+export const LandContainer = styled.div`
+  padding-top: 30vh;
+  background-image: url(${img});
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 100vh;
+`;
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,18 +1,26 @@
 import React from "react";
 // import city from '../images/bw-city.jpg';
-
+import { H1, LandContainer } from "../../src/components/styling/style";
 
 const Landing = () => {
-    return(
-        <div>
-        {/* Image not rendering at the moment - needs fixed */}
-        {/* styles={{ backgroundImage:`(${city})`}} */}
-        <header>
-        <h1>Show me the city!</h1>
-        </header>
-        <input type="text" placeholder="Search for a city.." name="search"></input>
-        </div>
-    )
-}
+  return (
+    <LandContainer className="text-center">
+      {/* Image not rendering at the moment - needs fixed */}
+      {/* styles={{ backgroundImage:`(${city})`}} */}
+      <header>
+        <H1>
+          Show me
+          <br></br>
+          the city!
+        </H1>
+      </header>
+      <input
+        type="text"
+        placeholder="Search for a city.."
+        name="search"
+      ></input>
+    </LandContainer>
+  );
+};
 
 export default Landing;
