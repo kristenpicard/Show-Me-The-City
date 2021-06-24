3a5c365de173ca38106d9d8c27a4f4d51a489ac8
Kristen Picard <kristenpicard93@gmail.com>
2021-06-24 12:58:13 -0400

    Styling view rec and view fave pages

--- src/pages/ViewFavorite.js
+++ src/pages/ViewFavorite.js
@@ -1,37 +1,63 @@
 import React from "react";
+import NavBar from "../components/NavBar/index";
+import {
+  DelBtn,
+  EditBtn,
+  Fave,
+  ImgRec,
+  RecBody,
+  RecContainer,
+  RecTitle,
+  ViewContainer,
+} from "../components/styling/style";
 // import "./style.css";
 import bbqImage from "../images/bbqplaceholder.png";
-import { GeneralContainer } from "../components/styling/style";
-import NavBar from "../components/NavBar/index"
 
 const ViewFavorite = () => {
   return (
     <>
-    <NavBar />
-    <GeneralContainer>
-      <a href="backapage">Back to Profile</a>
-      <img src={bbqImage} alt="bbq"></img>
-      <h1>Best BBQ!</h1>
-      <button>Favorite</button>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc aliquet
-        diam tortor, id consequat mauris ullamcorper eu. Orci varius natoque
-        penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-      </p>
-      <ul>
-        <li>1. Lorem ipsum dolor sit amet</li>
-        <li>2. consectetur adipiscing elit</li>
-        <li>3. sed do eiusmod tempor</li>
-        <li>4. incididunt ut labore et dolore</li>
-        <li>5. Lorem ipsum dolor sit amet</li>
-        <li>6. consectetur adipiscing elit</li>
-        <li>7. sed do eiusmod tempor</li>
-        <li>8. incididunt ut laboreet dolore</li>
-        <li>9. Lorem ipsum dolor sit amet</li>
-        <li>10. consectetur adipiscing elit</li>
-      </ul>
-      <button>Share</button>
-    </GeneralContainer>
+      <NavBar />
+      <ViewContainer>
+        <a href="backapage">Back to Profile</a>
+        <div className="container">
+          <br></br>
+          <ImgRec src={bbqImage} alt="bbq"></ImgRec>
+          <RecContainer>
+            <br></br>
+            <div className="row text-center">
+              <RecTitle className="col">Best BBQ!</RecTitle>
+              <Fave href="test" className="col">
+                500 ✰
+              </Fave>
+            </div>
+
+            <RecBody className="container">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+                aliquet diam tortor, id consequat mauris ullamcorper eu. Orci
+                varius natoque penatibus et magnis dis parturient montes,
+                nascetur ridiculus mus.
+              </p>
+              <ol>
+                <li>Lorem ipsum dolor sit amet</li>
+                <li>consectetur adipiscing elit</li>
+                <li>sed do eiusmod tempor</li>
+                <li>incididunt ut labore et dolore</li>
+                <li>Lorem ipsum dolor sit amet</li>
+                <li>consectetur adipiscing elit</li>
+                <li>sed do eiusmod tempor</li>
+                <li>incididunt ut laboreet dolore</li>
+                <li>Lorem ipsum dolor sit amet</li>
+                <li>consectetur adipiscing elit</li>
+              </ol>
+              <div className="text-center">
+                <EditBtn>Share</EditBtn>
+                
+              </div>
+            </RecBody>
+          </RecContainer>
+        </div>
+      </ViewContainer>
     </>
   );
 };
