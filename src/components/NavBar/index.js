78ae3f3417a243c04d4d37f0a1b0a685a7d281b1
Jake <jakech617@gmail.com>
2021-06-25 14:02:27 -0400

    changed the path for logging in to the homepage, and also added logout functionality to redirect to the landing page

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,26 +1,53 @@
-import React from "react";
-import {Nav} from "../styling/style"
+import React, { useState } from "react";
+import { Nav } from "../styling/style";
+import { Link, useHistory } from "react-router-dom";
+import { useAuth } from "../../contexts/AuthContext";
 
 function NavBar(props) {
-    return (
-                <Nav className="navbar">
-                {/* user ternary here */}
-                <div>
-                    {/* if user not logged in */}
-                    <a className="login" href="/login">Login</a>
-                </div>
-                <div>
-                    {/* if logged in */}
-                    <a className="profile" href="/profile">Profile</a>
-                    <a className="logout" href="/">Logout</a>
-                </div>
-                <div>
-                    {/* if on /profile */}
-                    <a className="home" href="/home">Home</a>
-                    <a className="logout" href="/">Logout</a>
-                </div>
-                </Nav>
-    )
+  const [error, setError] = useState("");
+  const { currentUser, logout } = useAuth();
+  const history = useHistory();
+
+  async function handleLogout() {
+    setError("");
+
+    try {
+      await logout()
+      history.push('/Login');
+    } catch {
+      setError("Failed to log out");
+    }
+  }
+
+  return (
+    <Nav className="navbar">
+      {/* user ternary here */}
+      <div>
+        {/* if user not logged in */}
+        <a className="login" href="/login">
+          Login
+        </a>
+      </div>
+      <div>
+        {/* if logged in */}
+        <a className="profile" href="/profile">
+          Profile
+        </a>
+        <a className="logout" href="/">
+          Logout
+        </a>
+      </div>
+      <div>
+        {/* if on /profile */}
+        <a className="home" href="/home">
+          Home
+        </a>
+        <a className="logout" href="/">
+          Logout
+        </a>
+      </div>
+    </Nav>
+  );
 }
 
 export default NavBar;
