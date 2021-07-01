185338a6eef25f1aa98363d1d821892c632d20e8
Janeé Mays <janee.elise@gmail.com>
2021-07-01 18:38:14 -0400

    pseudo code for search query

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,10 +6,38 @@ import SortResults from "../components/SortResults";
 import { GeneralContainer } from "../components/styling/style";
 
 const Home = () => {
+
+  // searchRec = (query) => {
+  //   API.getRec(query)
+  //     .then((res) =>
+  //       this.setState({
+  //         recs: res.data.items.map((recData) => this.makeRec(recData)),
+  //       })
+  //     )
+  //     .catch((err) => console.error(err));
+  // };
+
+  // handleInputChange = (event) => {
+  //   const name = event.target.name;
+  //   const value = event.target.value;
+  //   this.setState({
+  //     [name]: value,
+  //   });
+  // };
+
+  // handleFormSubmit = (event) => {
+  //   event.preventDefault();
+  //   this.searchRec(this.state.search);
+  // };
+
   return (
     <>
       <NavBar />
-      <SearchBox />
+      <SearchBox 
+        // search={this.state.search}
+        // handleInputChange={this.handleInputChange}
+        // handleFormSubmit={this.handleFormSubmit}
+      />
       <GeneralContainer className="text-center">
         <SortResults />
         <CardCarousel />
