c085db6026ec5941d079abdfdc7a138d03ffa9cd
kelsie-c <76745890+kelsie-c@users.noreply.github.com>
2021-08-16 17:44:53 -0400

    updated user schema

--- models/user.js
+++ models/user.js
@@ -3,37 +3,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
   
 const UserSchema = new Schema({
-  firstName: {
+  id: {
     type: String,
-    trim: true,
-    required: "First Name is Required",
   },
-
-  lastName: {
-    type: String,
-    trim: true,
-    required: "Last Name is Required",
-  },
-
-  password: {
-    type: String,
-    trim: true,
-    required: "Password is Required",
-    validate: [({ length }) => length >= 6, "Password should be longer."],
-  },
-
-  email: {
-    type: String,
-    unique: true,
-    match: [/.+@.+\..+/, "Please enter a valid e-mail address"],
-  },
-  // This is referencing the recommendations schema for user use
-  recs: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Recs",
-    },
-  ],
 
   faves: [
     {
@@ -41,15 +13,7 @@ const UserSchema = new Schema({
       ref: "Recs",
     },
   ],
-
-  fullName: String,
 });
-
-UserSchema.methods.setFullName = function () {
-  this.fullName = `${this.firstName} ${this.lastName}`;
-
-  return this.fullName;
-};
 
 const User = mongoose.model("User", UserSchema);
 
